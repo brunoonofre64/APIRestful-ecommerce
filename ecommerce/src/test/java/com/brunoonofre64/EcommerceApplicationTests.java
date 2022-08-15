e74925fae389860ab7b9b6a6a2ea3a75171d15e2
Bruno Onofre <85837638+brunoonofre64@users.noreply.github.com>
2022-08-15 10:31:57 -0300

    first commit

---
+++ ecommerce/src/test/java/com/brunoonofre64/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunoonofre64;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
